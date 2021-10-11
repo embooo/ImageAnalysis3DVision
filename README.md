ba68bbd6d0892a84214ee41502d8b62d82afd25d
embooo <79663490+embooo@users.noreply.github.com>
2021-10-11 03:57:58 +0200

    fin TP1 : hysteresis threshold, non-max suppr

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TP-AnalyseImage
 
+On Windows, execute buildVS2019.bat to generate de project files for Visual Studio 2019 with premake.
+You can modify this script to generate for another IDE : https://premake.github.io/docs/Using-Premake/
+
+Once built, the binary is located in the bin folder located at the root of the archive.
+To execute, either use a command prompt and specify a file e.g tp1.exe <image.format> or drag and drop an image file into the executable.
+
+The different image operations are under the ImOp namespace in TP1.cpp.
+
--- core/src/tp1/TP1.cpp
+++ core/src/tp1/TP1.cpp
@@ -49,10 +49,8 @@ namespace ImOp
             }
         }
 
-        //return value;
-        return std::max(0.0, std::min(value, 255.0));
-        // Normalize value to be -255 - 255 range
-        // Normalize value to be 0 - 255 range if image
+        return value;
+
         // amplitude
         // orientation/pente du gradient = theta arctan(grad(y)/grad(x))
     }
@@ -118,39 +116,110 @@ namespace ImOp
         return outImg;
     }
 
-    cv::Mat computeGradientOrientation(cv::Mat& Gx, cv::Mat& Gy)
+    cv::Mat computeAmplitudeGradient(cv::Mat& G, char dir)
     {
-        cv::Mat out(Gx.rows, Gx.cols, CV_64F);
-        Gx.convertTo(Gx, CV_64F);
-        Gy.convertTo(Gy, CV_64F);
+        // Single direction
+        cv::Mat out(G.rows, G.cols, CV_64F);
+        G.convertTo(G, CV_64F);
 
-        for(int x = 0; x < Gx.rows; ++x)
+        assert((dir == 'X') || (dir == 'Y'));
+        for (int x = 0; x < G.rows; ++x)
         {
-            for(int y = 0; y < Gx.cols; ++y)
+            for (int y = 0; y < G.cols; ++y)
             {
-                out.at<double>(x,y) = direction(Gx.at<double>(x,y), Gy.at<double>(x,y));
+                if (dir == 'X')
+                {
+                    out.at<double>(x, y) = amplitude(G.at<double>(x, y), 0.0);
+                }
+                else 
+                {
+                    out.at<double>(x, y) = amplitude(0.0, G.at<double>(x, y));
+                }
             }
         }
 
         return out;
     }
 
-	cv::Mat computeGradientAmplitude(cv::Mat& Gx, cv::Mat& Gy)
-	{
-		cv::Mat out(Gx.rows, Gx.cols, CV_64F);
-		Gx.convertTo(Gx, CV_64F);
-		Gy.convertTo(Gy, CV_64F);
+    cv::Mat computeGradientDirection(cv::Mat& Gx, cv::Mat& Gy)
+    {
+        cv::Mat out(Gx.rows, Gx.cols, CV_64F);
+        Gx.convertTo(Gx, CV_64F);
+        Gy.convertTo(Gy, CV_64F);
+        for (int x = 0; x < Gx.rows; ++x)
+        {
+            for (int y = 0; y < Gx.cols; ++y)
+            {
+                if (Gx.at<double>(x, y) < 0)
+                {
+                    out.at<double>(x, y) = -direction(Gx.at<double>(x, y), Gy.at<double>(x, y));
+                }
+                else
+                {
+                    out.at<double>(x, y) = direction(Gx.at<double>(x, y), Gy.at<double>(x, y));
+                }
+            }
+        }
 
-		for (int x = 0; x < Gx.rows; ++x)
-		{
-			for (int y = 0; y < Gx.cols; ++y)
-			{
-				out.at<double>(x, y) = amplitude(Gx.at<double>(x, y), Gy.at<double>(x, y));
-			}
-		}
+        return out;
+    }
 
-		return out;
-	}
+    cv::Mat computeGradientDirectionMD(cv::Mat& Gx, cv::Mat& Gy, cv::Mat& G_NW, cv::Mat& G_NE)
+    {
+        cv::Mat out(Gx.rows, Gx.cols, CV_64F);
+        Gx.convertTo(Gx, CV_64F);
+        Gy.convertTo(Gy, CV_64F);
+        for (int x = 0; x < Gx.rows; ++x)
+        {
+            for (int y = 0; y < Gx.cols; ++y)
+            {
+                out.at<double>(x, y) = std::max({ Gx.at<double>(x, y), Gy.at<double>(x, y), G_NW.at<double>(x, y), G_NE.at<double>(x, y) });
+            }
+        }
+
+        return out;
+    }
+
+    cv::Mat computeAmplitudeGBD(cv::Mat& Gx, cv::Mat& Gy)
+    {
+        cv::Mat out(Gx.rows, Gx.cols, CV_64F);
+        Gx.convertTo(Gx, CV_64F);
+        Gy.convertTo(Gy, CV_64F);
+        for(int x = 0; x < Gx.rows; ++x)
+        {
+            for(int y = 0; y < Gx.cols; ++y)
+            {
+                out.at<double>(x, y) = amplitude(Gx.at<double>(x, y), Gy.at<double>(x, y));
+            }
+        }
+
+        return out;
+    }
+
+    cv::Mat computeAmplitudeGMD(cv::Mat& Gx, cv::Mat& Gy, cv::Mat& G_NW, cv::Mat& G_NE)
+    {
+        // Module of multi directional gradient
+        // G_NW : gradient in the north-west direction, 3pi/4
+        // G_NE : gradient in the north-east direction, pi/4
+
+        cv::Mat out(Gx.rows, Gx.cols, CV_64F);
+        Gx.convertTo(Gx, CV_64F);
+        Gy.convertTo(Gy, CV_64F);
+        G_NW.convertTo(Gx, CV_64F);
+        G_NE.convertTo(Gy, CV_64F);
+
+
+        for (int x = 0; x < Gx.rows; ++x)
+        {
+            for (int y = 0; y < Gx.cols; ++y)
+            {
+                out.at<double>(x, y) = std::max({ Gx.at<double>(x, y), Gy.at<double>(x, y), 
+                                                  G_NW.at<double>(x, y), G_NE.at<double>(x, y)});
+            }
+        }
+
+        return out;
+    }
     
     /// <summary>
     /// Applies a global threshold to an image based on the value of the gradient at each pixel
@@ -158,7 +227,7 @@ namespace ImOp
     /// <param name="imAmplitude"> Matrix containing the amplitude of a the gradient for each pixel </param>
     /// <param name="threshold"> Arbitrary value </param>
     /// <returns> Image with a local threshold applied </returns>
-    cv::Mat globalThreshold(cv::Mat& imAmplitude, double threshold)
+    cv::Mat globalThreshold(const cv::Mat& imAmplitude, double threshold)
     {
         // Return an image
         cv::Mat outImg(imAmplitude.rows, imAmplitude.cols, CV_8UC1);
@@ -167,7 +236,7 @@ namespace ImOp
         {
             for(int y = 0; y < imAmplitude.cols; ++y)
             {
-                double& value = imAmplitude.at<double>(x, y);
+                const double& value = imAmplitude.at<double>(x, y);
                 unsigned char &grayValue = outImg.at<uchar>(x, y);
                 if(value < threshold)
                 {
@@ -190,14 +259,14 @@ namespace ImOp
     /// <param name="imAmplitude"> Matrix containing the amplitude of a the gradient for each pixel </param>
     /// <param name="filterDimension"> Size of the neighborhood to compute the local threshold </param>
     /// <returns> Image with a global threshold applied </returns>
-    cv::Mat localThreshold(cv::Mat& gradientAmp, const cv::Size& filterDimension)
+    cv::Mat localThreshold(const cv::Mat& gradientAmp, const cv::Size& filterDimension)
     {
         // Filter dimensions define the extent of the neighborhood 
         // to compute a local threshold
         
         cv::Mat outImg(gradientAmp.rows, gradientAmp.cols, CV_8UC1);
         cv::Mat meanFilter(filterDimension, CV_64F);
-        meanFilter = 1.0 / (filterDimension.height * filterDimension.width);
+        meanFilter = 1.0 / (filterDimension.height * filterDimension.width) ;
 
         const int filterHalf = floor(filterDimension.width / 2);
 
@@ -205,8 +274,8 @@ namespace ImOp
         {
             for (int y = filterHalf; y < gradientAmp.cols - filterHalf; ++y)
             {
-                double& value = gradientAmp.at<double>(x, y);
-                double localMean = conv(gradientAmp, meanFilter, cv::Point(x, y));
+                const double& value      = gradientAmp.at<double>(x, y);
+                double localMean         = conv(gradientAmp, meanFilter, cv::Point(x, y));
                 unsigned char& grayValue = outImg.at<uchar>(x, y);
 
                 if (value < localMean)
@@ -224,56 +293,194 @@ namespace ImOp
         return outImg;
     }
 
+    
+    cv::Mat hysteresisThreshold(const cv::Mat& gradientAmp, cv::Size neighbordhoodSize, double sh, double factor)
+    {
+        assert(factor <= 1.0, ("Low threshold is higher than high threshold"));
+        // Relation between low and high threshold
+        double sb = factor * sh;
+
+
+        cv::Mat outImg = cv::Mat(gradientAmp.size(), CV_8UC1);
+        cv::Mat neighborhood(neighbordhoodSize, CV_64F);
+        int half = floor(neighbordhoodSize.width / 2);
+        for (int x = half; x < outImg.rows - half; ++x)
+        {
+            for (int y = half; y < outImg.cols - half; ++y)
+            {
+                if (gradientAmp.at<double>(x, y) < sb)
+                {
+                    outImg.at<uchar>(x, y) = 0;
+                }
+                else if (gradientAmp.at<double>(x, y)  > sh)
+                {
+                    outImg.at<uchar>(x, y) = 255;
+                }
+                else
+                {
+                    // Check neighbors of pixels having an amplitude between sb and sh
+                    bool notfound = true;
+                    for (int fx = -half; fx <= half; ++fx)
+                    {
+                        for (int fy = -half; fy <= half; ++fy)
+                        {
+                            if (gradientAmp.at<double>(x + fx, y + fy)  > sh)
+                            {
+                                outImg.at<uchar>(x + fx, y + fy) = 255;
+                                notfound = false;
+                            }
+                            else
+                            {
+                                outImg.at<uchar>(x, y) = 0;
+                            }
+                            if (!notfound) break;
+                        }
+                        if (!notfound) break;
+                    }
+                }
+            }
+        }
+
+        return outImg;
+    }
+
+    
+
+    cv::Mat affinage(const cv::Mat& gradientAmp, const cv::Mat& gradientDir)
+    {
+        cv::Mat outImg = cv::Mat(gradientAmp.size(), CV_64F);
+        
+        for (int x = 1; x < outImg.rows - 1; ++x)
+        {
+            for (int y = 1; y < outImg.cols - 1; ++y)
+            {
+                double angle = gradientDir.at<double>(x, y);
+                if (angle < 0) angle += 180.0;
+                double q = 1.0;
+                double r = 1.0;
+
+                // select the neighbors in the direction of gradient
+                if ((0 <= angle && angle < 22.5) || (157.5 <= angle && angle <= 180))
+                {
+                    q = gradientAmp.at<double>(x, y + 1);
+                    r = gradientAmp.at<double>(x, y - 1);
+                }
+                else if (22.5 <= angle && angle < 67.5)
+                {
+                    q = gradientAmp.at<double>(x + 1, y - 1);
+                    r = gradientAmp.at<double>(x - 1, y + 1);
+                }
+                else if (67.5 <= angle && angle < 112.5)
+                {
+                    q = gradientAmp.at<double>(x + 1, y);
+                    r = gradientAmp.at<double>(x - 1, y);
+                }
+                else if (112.5 <= angle && angle < 157.5)
+                {
+                    q = gradientAmp.at<double>(x - 1, y - 1);
+                    r = gradientAmp.at<double>(x + 1, y + 1);
+                }
+
+
+                // check if central pixel is local max
+                double centralVal = gradientAmp.at<double>(x, y)  ;
+                if (centralVal >= q && centralVal >= r)
+                {
+                    outImg.at<double>(x, y) = centralVal ;
+                }
+                else
+                {
+                    outImg.at<double>(x, y) = 0.0;
+                }
+
+            }
+
+        }
+
+
+        return outImg;
+    }
+
+
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    // Initialize window
-    WinProps resultWindow("Result", {800, 600});
-    cv::namedWindow(resultWindow.name());
+    // Read file
+    if (argc < 2)
+    {
+        std::cerr << "Usage : " << argv[0] << " <image.format> ." << std::endl;
+    }
+    else
+    {
+        // Initialize window
+        WinProps resultWindow("Result", { 800, 600 });
+        cv::namedWindow(resultWindow.name());
 
-    // Read source image
-    cv::Mat img = ImIO::readImg("../../data/img/lena_std.tif");
+        // Read source image
+        cv::Mat img = ImIO::readImg(argv[1]);
+        cv::Mat orig;
+        img.copyTo(orig);
+        // 3x3 directional filters
 
-    // 3x3 directional filters
+        cv::Mat gradientX = (cv::Mat_<double>(3, 3) << /* R0 */ -1.0, 0.0, 1.0,   /* R1 */ -1.0, 0.0, 1.0,  /* R2 */-1.0, 0.0, 1.0) * 1.0 / 3.0;
+        cv::Mat gradientY = (cv::Mat_<double>(3, 3) << /* R0 */-1.0, -1.0, -1.0,   /* R1 */ 0.0, 0.0, 0.0,  /* R2 */  1.0, 1.0, 1.0) * 1.0 / 3.0;
+        cv::Mat gradientNE = (cv::Mat_<double>(3, 3) << /* R0 */ 1.0, 1.0, 0.0,   /* R1 */ 1.0, 0.0, -1.0,  /* R2 */ 0.0, -1.0, -1.0) * 1.0 / 3.0;
+        cv::Mat gradientNW = (cv::Mat_<double>(3, 3) << /* R0 */ 0.0, 1.0, 1.0,   /* R1 */-1.0, 0.0, 1.0,  /* R2 */ -1.0, -1.0, 0.0) * 1.0 / 3.0;
 
-    cv::Mat gradientX    = (cv::Mat_<double>(3,3) << /* R0 */ -1.0, 0.0, 1.0,   /* R1 */ -1.0, 0.0, 1.0,  /* R2 */-1.0, 0.0, 1.0)  * 1.0 / 3.0; // Prewitt filter
-    cv::Mat gradientY    = (cv::Mat_<double>(3,3) << /* R0 */-1.0,-1.0,-1.0,   /* R1 */ 0.0, 0.0, 0.0,  /* R2 */  1.0,  1.0,  1.0) * 1.0 / 3.0;
-    cv::Mat gradientDpos = (cv::Mat_<double>(3,3) << /* R0 */ 1.0, 1.0, 0.0,   /* R1 */ 1.0, 0.0, -1.0,  /* R2 */ 0.0, -1.0, -1.0) * 1.0 / 3.0;
-    cv::Mat gradientDneg = (cv::Mat_<double>(3,3) << /* R0 */ 0.0, 1.0, 1.0,   /* R1 */-1.0, 0.0, 1.0,  /* R2 */ -1.0, -1.0, 0.0)  * 1.0 / 3.0;
+        cv::Mat sobel = (cv::Mat_<double>(3, 3) << /* R0 */ -1.0, 0.0, 1.0, /* R1 */ -2.0, 0.0, 2.0,  /* R2 */ -1.0, 0.0, 1.0) * 1.0 / 4.0;
+        cv::Mat kirsch4 = (cv::Mat_<double>(3, 3) << /* R0 */ -3.0, -3.0, -3.0, /* R1 */ 5.0, 0.0, -3.0, /* R2 */ 5.0, 5.0, -3.0) * 1.0 / 15.0;
 
-    cv::Mat sobel        = (cv::Mat_<double>(3,3) << /* R0 */ -1.0,  0.0,  1.0, /* R1 */ -2.0, 0.0, 2.0,  /* R2 */ -1.0, 0.0, 1.0)  * 1.0 / 4.0;
-    cv::Mat kirsch4      = (cv::Mat_<double>(3,3) << /* R0 */ -3.0, -3.0, -3.0, /* R1 */ 5.0,  0.0, -3.0, /* R2 */ 5.0,  5.0, -3.0) * 1.0 / 15.0;
+        // Apply convolution
+        cv::Mat outGradientX = ImOp::conv2d(img, gradientX, false);
+        cv::Mat outGradientY = ImOp::conv2d(img, gradientY, false);
+        cv::Mat outGradientNW = ImOp::conv2d(img, gradientNW, false); // north west direction, pi/4
+        cv::Mat outGradientNE = ImOp::conv2d(img, gradientNE, false); // north west direction, 3pi/4
 
-    // Apply convolution
-    cv::Mat outGradientX       = ImOp::conv2d(img, gradientX, false);
-    cv::Mat outGradientY       = ImOp::conv2d(img, gradientY, false);
-    cv::Mat outGradientDpos    = ImOp::conv2d(img, gradientDpos, false);
-    cv::Mat outGradientDneg    = ImOp::conv2d(img, gradientDneg, false);
-
-    cv::Mat outSobel           = ImOp::conv2d(img, sobel, true);
-    cv::Mat outKirsch4         = ImOp::conv2d(img, kirsch4, false);
+        cv::Mat outSobel = ImOp::conv2d(img, sobel, true);
+        cv::Mat outKirsch4 = ImOp::conv2d(img, kirsch4, false);
 
 
-    // Threshold
-    cv::Mat amplitudeBiDirectional = ImOp::computeGradientAmplitude(outGradientX, outGradientY);
+        // Threshold
+        cv::Mat amplitudeGradientX = ImOp::computeAmplitudeGradient(outGradientX, 'X');
+        cv::Mat amplitudeGradientY = ImOp::computeAmplitudeGradient(outGradientY, 'Y');
+        cv::Mat amplitudeBiDirectional = ImOp::computeAmplitudeGBD(outGradientX, outGradientY);
+        cv::Mat amplitudeMultiDirectional = ImOp::computeAmplitudeGMD(outGradientX, outGradientY, outGradientNW, outGradientNE);
 
-    cv::Mat outGlobalThreshold     = ImOp::globalThreshold(amplitudeBiDirectional, 0.1);
-    cv::Mat outLocalThreshold      = ImOp::localThreshold(amplitudeBiDirectional, cv::Size(3,3));
 
-    // Optional : concatenate results into a single mat
-    std::vector<cv::Mat> outMats      {outGradientX, outSobel, outKirsch4, outGradientY };
-    std::vector<cv::Mat> outGradients {outGradientX, outGradientY, outGradientDpos, outGradientDneg};
+        cv::Mat outGlobalThreshold = ImOp::globalThreshold(amplitudeBiDirectional, 0.1);
+        cv::Mat outLocalThreshold = ImOp::localThreshold(amplitudeBiDirectional, cv::Size(11, 11));
+        cv::Mat outHysteresis = ImOp::hysteresisThreshold(amplitudeBiDirectional, cv::Size(3, 3), 0.13, 0.5);
 
-    cv::Mat outGradientsConcat;                            
-    cv::hconcat(outGradients, outGradientsConcat); 
+        // Optional : concatenate results into a single mat
+        std::vector<cv::Mat> outMats{ outGradientX, outSobel, outKirsch4, outGradientY };
+        std::vector<cv::Mat> outGradients{ amplitudeGradientX, amplitudeGradientY, amplitudeBiDirectional, amplitudeMultiDirectional };
+        std::vector<cv::Mat> outThresholds{ outGlobalThreshold, outLocalThreshold, outHysteresis };
 
-    //ImOp::computeDirection(outGradientX, outGradientY);
-    // Display result
-    imshow(resultWindow.name(), outLocalThreshold);
-    
-    // Wait for a keystroke before terminating
-    int key = cv::waitKey(0); 
+        cv::Mat outGradientsConcat;
+        cv::hconcat(outGradients, outGradientsConcat);
+
+        cv::Mat outThresholdsConcat;
+        cv::hconcat(outThresholds, outThresholdsConcat);
+
+        // Affinage
+
+        cv::Mat directionBiDirectional = ImOp::computeGradientDirection(outGradientX, outGradientY);
+        cv::Mat affin = ImOp::affinage(amplitudeBiDirectional, directionBiDirectional);
+
+
+        cv::Mat affinThresh = ImOp::hysteresisThreshold(affin, cv::Size(3, 3), 0.13, 0.5);
+
+        // Display original image
+        imshow("Source Image", orig);
+        // Display result
+        imshow(resultWindow.name(), affinThresh);
+
+        // Wait for a keystroke before terminating
+        int key = cv::waitKey(0);
+    }
+
+
 
     return 0;
 }
